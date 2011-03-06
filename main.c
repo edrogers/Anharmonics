e3f9c419817ecf6744e485db6585e850774418d2
ed <>
2011-03-06 04:28:22 +0000

    Gave an interesting, non-trivial challenge: 4*lambda*BABA.  Seemed to work okay.

--- main.c
+++ main.c
@@ -1,16 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include "Coefficient.h"
-#include "Term.h"
 #include "Polynomial.h"
+#include "Term.h"
+#include "Coefficient.h"
 
 using namespace std;
 
 int main()
 {
 
-  
+  vector<unsigned int> alphas;
+  Coefficient firstCoefficient(4.0,alphas);
+
+  vector<Coefficient> firstTermCoefficients;
+  firstTermCoefficients.push_back(firstCoefficient);
+
+  vector<char> firstTermOperators;
+  firstTermOperators.push_back('B');
+  firstTermOperators.push_back('A');
+  firstTermOperators.push_back('B');
+  firstTermOperators.push_back('A');
+
+  Term firstTerm(firstTermOperators,
+		 firstTermCoefficients,
+		 1);
+
+  vector<Term> normalOrderedTerms;
+  normalOrderedTerms = firstTerm.normalOrder();
+
+  Polynomial firstPolynomial(normalOrderedTerms);
+
+  firstPolynomial.simplify();
+
+  /* vector<Term> unorderedTerms; */
+  /* unorderedTerms.push_back(firstTerm); */
+
+  /* Polynomial firstPolynomial(unorderedTerms); */
+
+  firstPolynomial.print();
+  cout << endl;
 
   return 0;
 }

e0278e02f89e5def2ae95400b21358a358a519b3
ed <>
2011-03-07 21:53:32 +0000

    Added constructor for Term with std::string as input parameter.  Fixing small bug in print() method.

--- Term.c
+++ Term.c
@@ -33,6 +33,126 @@ Term::Term(vector<char> operators,
     }
 }
 
+Term::Term(string inputString)
+{
+  //Proper formatting:
+  //  lambda^2*(2*a_1*a_2^3*a_3+7*a_3^2)*ABABA
+  //  (2*a_2)*BBAAAA
+  //  lambda*AB
+  //  lambda^3*(2-7*a_1)
+
+  //Improper formatting:
+  //  (2*a_1*(a_2+3*a_3))*ABABA  // should be (2*a_1*a_2+6*a_1*a_3)*ABABA
+  //  3*lambda*BABA              // should be lambda*(3)*BABA
+  //  5*A                        // should be (5)*A
+
+  // first: no parenthesis --> no coefficients (just 1.0)
+  //           parenthesis --> coefficient(s) contained
+  size_t openParenthesisLocation = inputString.find('(');
+  string coefficientsStringFull = "";
+  if (openParenthesisLocation != string::npos)
+    {
+      // find the close parenthesis.  Extract the coefficient(s) substring.
+      size_t closeParenthesisLocation = inputString.find(')', openParenthesisLocation+1);
+      if (closeParenthesisLocation == string::npos) 
+	{
+	  cout << "ERROR: Mismatched parenthesis in Term constructor: " << inputString << endl;
+	}
+      else
+	{
+	  coefficientsStringFull = inputString.substr(openParenthesisLocation+1,
+						      closeParenthesisLocation-openParenthesisLocation-1);
+
+	  // separate the string into coefficients, starting from the back.
+	  fCoefficients.clear();
+
+	  while (coefficientsStringFull.length() > 0)
+	    {
+	      size_t lastPlusSignPosition  = coefficientsStringFull.rfind('+');
+	      size_t lastMinusSignPosition = coefficientsStringFull.rfind('-');
+	      size_t coefficientLength = coefficientsStringFull.length();
+	      size_t coefficientPosition = 0;
+
+	      if  (lastPlusSignPosition  != string::npos)
+		{
+		  coefficientLength = coefficientsStringFull.length()-lastPlusSignPosition;
+		  coefficientPosition = lastPlusSignPosition;
+		}
+	      if ((lastMinusSignPosition != string::npos) && 
+		  (lastMinusSignPosition > lastPlusSignPosition))
+		{
+		  coefficientLength = coefficientsStringFull.length()-lastMinusSignPosition;
+		  coefficientPosition = lastMinusSignPosition;
+		}
+
+	      Coefficient latestCoefficient(coefficientsStringFull.substr(coefficientPosition,coefficientLength));
+	      fCoefficients.push_back(latestCoefficient);
+	      coefficientsStringFull.resize(coefficientPosition);
+	    }
+	  reverse(fCoefficients.begin(),fCoefficients.end());
+
+	  inputString.replace(openParenthesisLocation,closeParenthesisLocation-openParenthesisLocation+1,"");
+	  size_t duplicateStarLocation = inputString.find("**");
+	  if (duplicateStarLocation != string::npos) 
+	    {
+	      inputString.replace(duplicateStarLocation,2,"*");
+	    }
+	  else if (inputString.at(0) == '*')
+	    {
+	      inputString.replace(0,1,"");
+	    }
+	  else if (inputString.at(inputString.length()-1) == '*')
+	    {
+	      inputString.replace(0,1,"");
+	    }
+	}
+    }
+  else
+    {
+      // Coefficient == 1.0
+      fCoefficients.clear();
+      Coefficient unity;
+      fCoefficients.push_back(unity);
+    }
+
+  size_t lambdaLocation = inputString.find("lambda");
+  if (lambdaLocation != string::npos)
+    {
+      bool lambdaIsExponentiated = (inputString.at(lambdaLocation+6) == '^');
+      if (lambdaIsExponentiated)
+	{
+	  size_t endOfExponentiation = inputString.find('*',lambdaLocation+7);
+	  if (endOfExponentiation != string::npos)
+	    {
+	      string exponentiation = inputString.substr(lambdaLocation+7,endOfExponentiation-(lambdaLocation+7));
+	      inputString.replace(lambdaLocation,endOfExponentiation-lambdaLocation+1,"");
+	      fOrderInLambda = atoi(exponentiation.c_str());
+	    }
+	  else
+	    {
+	      string exponentiation = inputString.substr(lambdaLocation+7,endOfExponentiation);
+	      inputString.replace(lambdaLocation,endOfExponentiation,"");
+	      fOrderInLambda = atoi(exponentiation.c_str());
+	    }
+	}
+      else
+	{
+	  fOrderInLambda = 1;
+	  inputString.replace(lambdaLocation,6,"");
+	  if (inputString.at(0) == '*') inputString.replace(0,1,"");
+	}
+    }
+  else
+    {
+      fOrderInLambda = 0;
+    }
+
+  for (string::iterator it = inputString.begin();
+       it != inputString.end(); it++) 
+    {
+      fOperators.push_back(*it);
+    }
+}
 
 Term& Term::operator*=(const Term& rhs)
 {
@@ -142,6 +262,7 @@ vector<Term> Term::normalOrder()
   Term firstTerm(firstTermOperators,
 		 fCoefficients,
 		 fOrderInLambda);
+
   Term secondTerm(secondTermOperators,
 		  fCoefficients,
 		  fOrderInLambda);
@@ -229,8 +350,8 @@ void Term::print()
 	{
 	  cout << "^" << fOrderInLambda;
 	}
-      numberOfFactors--;
       if (numberOfFactors > 1) cout << "*";
+      numberOfFactors--;
     }
   if (!coefficientIsUnity)
     {
@@ -244,9 +365,9 @@ void Term::print()
 	    }
 	  it->print();
 	}
-      numberOfFactors--;
       cout << ")";
       if (numberOfFactors > 1) cout << "*";
+      numberOfFactors--;
     }
   if (fOperators.size() != 0) 
     {

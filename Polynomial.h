7503f68509f2809ce4dfff968c1c135dcb475d3c
ed <>
2011-03-13 19:24:03 +0000

    Updating for inclusion, identification, and use of DoubleTerm factors. Also adding ostream input to print methods.

--- Polynomial.h
+++ Polynomial.h
@@ -2,13 +2,17 @@
 #define POLYNOMIAL_H
 
 #include <vector>
+#include <fstream>
 #include <iostream>
+#include <algorithm>
 #include "Term.h"
+#include "DoubleTerm.h"
 
 class Polynomial
 {
  private:
   std::vector<Term> fTerms;
+  std::vector<DoubleTerm> fDoubleTerms;
 
  public:
   Polynomial(std::vector<Term> terms);
@@ -22,16 +26,21 @@ class Polynomial
   Polynomial& operator^=(const unsigned int& rhs);
   const Polynomial operator^(const unsigned int& rhs) const;
 
-  std::vector<Term> getTerms() const { return fTerms; };
+  std::vector<Term>       getTerms() const       { return fTerms; };
+  std::vector<DoubleTerm> getDoubleTerms() const { return fDoubleTerms; };
 
   const Polynomial commutedWith(const Polynomial& rhs) const;
 
+  void identifyDoubleTerms();
+  void ungroupDoubleTerms();
+
   void gatherTerms();
+  void groupTerms();
   void simplify();
   void normalOrder();
   
-  void print();
-  void printTex();
+  void print(std::ostream& outputStream = std::cout);
+  void printTex(std::ostream& outputStream = std::cout, unsigned int termsAtATime = 0, unsigned int termSetNum = 0);
 };
 
 #endif
